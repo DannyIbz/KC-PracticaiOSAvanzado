32137ef057366bbe85d5d1a17a1dda1387d62800
Daniel Sánchez <danisanchez86@gmail.com>
2017-03-05 14:29:56 +0100

    Adding decode function on BookDB+Utils

--- HackerBooksLite/App/Model/Utils/BookDB+Utils.swift
+++ HackerBooksLite/App/Model/Utils/BookDB+Utils.swift
@@ -6,8 +6,76 @@
 //  Copyright © 2017 KeepCoding. All rights reserved.
 //
 
-import Foundation
+import CoreData
 
 extension BookDB {
     
+    //MARK: - Decodification
+    func decode(book dict: JSONDictionary) throws -> BookDB{
+        
+        var context: NSManagedObjectContext
+        
+        // validate first
+        try validate(dictionary: dict)
+        
+        // extract from dict
+        func extract(key: String) -> String{
+            return dict[key]!   // we know it can't be missing because we validated first!
+        }
+        
+        // parsing
+        let authors = parseCommaSeparated(string: extract(key: "authors"))
+        let imgURL = URL(string: extract(key: "image_url"))!
+        let pdfURL = URL(string: extract(key: "pdf_url"))!
+        let tags = Tags(parseCommaSeparated(string: extract(key: "tags")).map{Tag(name: $0)})
+        let title = extract(key: "title").capitalized
+        
+        let mainBundle = Bundle.main
+        
+        let defaultImage = mainBundle.url(forResource: "emptyBookCover", withExtension: "png")!
+        let defaultPdf = mainBundle.url(forResource: "emptyPdf", withExtension: "pdf")!
+        
+        // AsyncData
+        let image = AsyncData(url: imgURL, defaultData: try! Data(contentsOf: defaultImage))
+        let pdf = AsyncData(url: pdfURL, defaultData: try! Data(contentsOf: defaultPdf))
+        
+        // Insert JSON data on BookDB entity
+        
+        
+        guard let context = self.context else { return }
+        saveContext(context: context)
+        
+    }
+    
+    func decode(book dict: JSONDictionary?) throws -> Book{
+        
+        guard let d = dict else {
+            throw JSONErrors.emptyJSONObject
+        }
+        return try decode(book:d)
+    }
+    
+    
+    //MARK: - Validation
+    // Validation should be kept waya from processing to
+    // insure the single responsability principle
+    // https://medium.com/swift-programming/why-swift-guard-should-be-avoided-484cfc2603c5#.bd8d7ad91
+    private
+    func validate(dictionary dict: JSONDictionary) throws{
+        
+        func isMissing() throws{
+            for key in dict.keys{
+                guard let value = dict[key] else{
+                    throw JSONErrors.missingField(name: key)
+                }
+                guard value.characters.count > 0  else {
+                    throw JSONErrors.incorrectValue(name: key, value: value)
+                }
+            }
+            
+        }
+        
+        try isMissing()
+        
+    }
 }
